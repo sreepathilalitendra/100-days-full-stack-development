7c1cedb0b320dce09e6f35150c5c765c55096c4a
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-21 04:58:43 +0530

    Added Day - 42 Java Script Variables

---
+++ Day-42-JavaScript-Variables/script.js
@@ -0,0 +1,40 @@
+// Day 42 - JavaScript Variables
+
+// var Keyword
+
+var studentName = "Rahul";
+
+console.log("Student Name:", studentName);
+
+// let Keyword
+
+let age = 20;
+
+console.log("Age:", age);
+
+// const Keyword
+
+const country = "India";
+
+console.log("Country:", country);
+
+// Updating Variables
+
+studentName = "Ramesh";
+
+age = 21;
+
+// const cannot be reassigned
+// country = "USA"; ❌
+
+// Display Updated Values
+
+console.log("Updated Student Name:", studentName);
+
+console.log("Updated Age:", age);
+
+console.log("Constant Country:", country);
+
+// End Message
+
+console.log("Day 42 Completed Successfully!");
