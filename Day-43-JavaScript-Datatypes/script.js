22097ed3a919fb95350fe31273ac82a3e5ba9fdd
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-21 05:03:39 +0530

    Added Day - 43 Java Script Datatypes

---
+++ Day-43-JavaScript-Datatypes/script.js
@@ -0,0 +1,77 @@
+// Day 43 - JavaScript Data Types
+
+// String
+
+let name = "CodeMonolith";
+
+// Number
+
+let age = 21;
+
+// Boolean
+
+let isStudent = true;
+
+// Undefined
+
+let city;
+
+// Null
+
+let mobile = null;
+
+// BigInt
+
+let population = 12345678901234567890n;
+
+// Symbol
+
+let id = Symbol("id");
+
+// Object
+
+let student = {
+    name: "Rahul",
+    age: 20,
+    course: "Full Stack Development"
+};
+
+// Display Values
+
+console.log("String:", name);
+
+console.log("Number:", age);
+
+console.log("Boolean:", isStudent);
+
+console.log("Undefined:", city);
+
+console.log("Null:", mobile);
+
+console.log("BigInt:", population);
+
+console.log("Symbol:", id);
+
+console.log("Object:", student);
+
+// Display Data Types
+
+console.log(typeof name);
+
+console.log(typeof age);
+
+console.log(typeof isStudent);
+
+console.log(typeof city);
+
+console.log(typeof mobile);
+
+console.log(typeof population);
+
+console.log(typeof id);
+
+console.log(typeof student);
+
+// End Message
+
+console.log("Day 43 Completed Successfully!");
