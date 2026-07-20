525c1bbc81d772448d95b62f87d0afde9801d6d3
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-21 04:54:26 +0530

    Added Day - 41 Java Script Introduction

---
+++ Day-41-JavaScript-Introduction/script.js
@@ -0,0 +1,23 @@
+// Day 41 - JavaScript Introduction
+
+// Display message in Browser Console
+
+console.log("Welcome to JavaScript!");
+
+// Display popup message
+
+alert("Welcome to JavaScript Full Stack Development Series!");
+
+// Display message in Web Console
+
+console.log("JavaScript makes web pages interactive.");
+
+console.log("HTML provides Structure.");
+
+console.log("CSS provides Styling.");
+
+console.log("JavaScript provides Functionality.");
+
+// End Message
+
+console.log("Day 41 Completed Successfully!");
