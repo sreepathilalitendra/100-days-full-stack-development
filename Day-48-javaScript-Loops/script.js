cc80726dda1a499026b1b89d33d0c3e79a7bc857
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-21 07:22:51 +0530

    Added Day - 48 Java Script Loops

---
+++ Day-48-javaScript-Loops/script.js
@@ -0,0 +1,57 @@
+// Day 48 - JavaScript Loops
+
+// for Loop
+
+console.log("for Loop");
+
+for (let i = 1; i <= 5; i++) {
+
+    console.log(i);
+
+}
+
+// while Loop
+
+console.log("while Loop");
+
+let a = 1;
+
+while (a <= 5) {
+
+    console.log(a);
+
+    a++;
+
+}
+
+// do...while Loop
+
+console.log("do...while Loop");
+
+let b = 1;
+
+do {
+
+    console.log(b);
+
+    b++;
+
+} while (b <= 5);
+
+// Nested Loop
+
+console.log("Nested Loop");
+
+for (let row = 1; row <= 3; row++) {
+
+    for (let column = 1; column <= 3; column++) {
+
+        console.log("Row:", row, "Column:", column);
+
+    }
+
+}
+
+// End Message
+
+console.log("Day 48 Completed Successfully!");
