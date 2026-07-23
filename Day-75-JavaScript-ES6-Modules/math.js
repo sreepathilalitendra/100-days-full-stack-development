cd0a9df7b20ef7b82616d48f6b4f32f1401bf317
Sreepathi Lalitendra <lalitendrasreepathi544@gmail.com>
2026-07-23 17:47:41 +0530

    Added Day - 75 JavaScript ES6 Modules

---
+++ Day-75-JavaScript-ES6-Modules/math.js
@@ -0,0 +1,15 @@
+// Day 75 - ES6 Module
+
+export function add(a, b) {
+
+    return a + b;
+
+}
+
+export function subtract(a, b) {
+
+    return a - b;
+
+}
+
+export const course = "Full Stack Development";
---
+++ Day-75-JavaScript-ES6-Modules/script.js
@@ -0,0 +1,15 @@
+// Day 75 - Import ES6 Module
+
+import { add, subtract, course } from "./math.js";
+
+console.log("Course:", course);
+
+console.log("----------------");
+
+console.log("Addition:", add(20, 10));
+
+console.log("Subtraction:", subtract(20, 10));
+
+console.log("----------------");
+
+console.log("Day 75 Completed Successfully!");
